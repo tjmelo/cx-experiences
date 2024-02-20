86c868ba2c294888dd73b671c0d53ca858b081d9
Thiago Melo <thiago.tmwd@gmail.com>
2024-02-20 07:26:19 -0300

    feat: chapter 7

---
+++ Node/cap.7/clientTCP.ts
@@ -0,0 +1,27 @@
+import { Socket } from 'net';
+
+const PORT = 3000;
+
+const client = new Socket();
+client.setEncoding('utf-8');
+
+//server connect
+client.connect(PORT, 'localhost', () => {
+    console.log('Connected to server...');
+    client.write('Who needs a browser to communicate?');
+});
+
+//get data, send us to server
+process.stdin.on('data', (data) => {
+    client.write(data);
+});
+
+//when get the data, send us to console
+client.on('data', (data) => {
+    console.log(data);
+});
+
+//when the server closes the connection
+client.on('close', () => {
+    console.log('Connection is closed.')
+})
---
+++ Node/cap.7/serverTCP.ts
@@ -0,0 +1,34 @@
+import { createServer } from 'net';
+
+const PORT = 3000;
+
+const server = createServer((conn) => {
+    console.log('Connected...');
+
+    conn.on('data', (data) => {
+        console.log(`${data} from ${conn.remoteAddress} ${conn.remotePort}`);
+
+        conn.write(`Repeating ${data}`);
+    });
+
+    conn.on('close', () => {
+        console.log('Client close connection...');
+    })
+}).listen(PORT);
+
+server.on('listening', () => {
+    console.log(`Listing on port ${PORT}`);
+});
+
+server.on('error', (err) => {
+    if(err.name == 'EADDRINUSE') {
+        console.warn('Address in use, retrying...');
+        
+        setTimeout(() => {
+            server.close();
+            server.listen(PORT);
+        }, 1000);
+    } else {
+        console.log(err);
+    }
+})
