2de62c2892761fcd62a04cbf02fc2ef2074de6f0
Thiago Melo <thiago.tmwd@gmail.com>
2024-02-21 06:12:43 -0300

    Update Node/cap.8/childProcess.ts
    Co-authored-by: TMWD <101524755+tm-wd@users.noreply.github.com>

--- Node/cap.8/childProcess.ts
+++ Node/cap.8/childProcess.ts
@@ -2,9 +2,7 @@ import { spawn } from "child_process";
 
 const pwd = spawn('pwd');
 
-pwd.stdout.on('data', (data) => {
-    console.log('stdout: ' + data);
-});
+pwd.stdout.on('data', data => console.log('stdout: ' + data));
 
 pwd.stderr.on('data', (data) => {
     console.error('stderr: ' + data);
